ab34b5c9edfbb45ae78b2c65c617cfbc223dc500
bochockito <bochockitoseminiano@gmail.com>
2026-07-15 00:54:46 +0800

    chore: menu and images

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,30 +1,71 @@
-import { Card, Button } from "antd";
+import { Card, Button, Tag, Image } from "antd";
+import { no_image } from "../assets/image/fallback";
 
 export default function MenuCard({item, onOrder}) {
+
+    const imageSrc = item.image ? `${import.meta.env.BASE_URL}${item.image}` : no_image;
+
     return (
-        <Card hoverablecover={item.image}>
+        <Card 
+            hoverable
+            cover={
+                <Image
+                    alt={item.name}
+                    width="100%"
+                    height="250px"
+                    src={imageSrc}
+                    preview={false}
+                    fallback={no_image}
+                    className="h-56 w-full object-cover"
+                />
+
+            }
+        >
             <h3 className="font-bold text-xl">
                 {item.name}
             </h3>
 
-            <p className="text-gray-600">
+            <div className="text-gray-600">
                 {item.description}
-            </p>
+            </div>
+
+            { item.japdesc != "" &&
+                <div className="text-gray-600 mt-[10px]">
+                    {item.japdesc}
+                </div>
+            }
+            <div className="mt-4">
+                <h4 className="font-semibold text-gray-700 mb-2">
+                    Order Type
+                </h4>
+                <div className="flex flex-wrap gap-1">
+                    {item.type?.map((ingredient, index) => (
+                        <Tag key={index} color="default">
+                            {ingredient}
+                        </Tag>
+                    ))}
+                </div>
+            </div>
+            <div className="mt-4">
+                <h4 className="font-semibold text-gray-700 mb-2">
+                    Ingredients
+                </h4>
+                <div className="flex flex-wrap gap-1">
+                    {item.ingredients?.map((ingredient, index) => (
+                        <Tag key={index} color="gold">
+                            {ingredient}
+                        </Tag>
+                    ))}
+                </div>
+            </div>
 
             <div className="mt-3 flex justify-between items-center">
-
                 <span className="font-bold text-red-600 text-lg">
-                    ₱{item.price}
+                    ₱{(item.price).toFixed(2)}
                 </span>
-
-                <Button
-                    type="primary"
-                    danger
-                    onClick={onOrder}
-                >
+                <Button type="primary" danger onClick={onOrder}>
                     Order Now
                 </Button>
-
             </div>
 
         </Card>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ export default function SearchBar({value, onChange}) {
     return (
         <Input
             size="large"
-            placeholder="🔍 Hanapin ang pagkain..."
+            placeholder="🔍 Hanapin ang pagkain - 食べ物を探してください - Find the food..."
             value={value}
             onChange={(e) => onChange(e.target.value)}
         />
